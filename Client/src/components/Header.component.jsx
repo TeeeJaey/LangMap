815b83d4aa8d441fa1946da10a68965f44c6a1af
Tejas Jadhav <tjadhav95@gmail.com>
2021-09-18 12:36:13 +0530

    Header updates

--- Client/src/components/Header.component.jsx
+++ Client/src/components/Header.component.jsx
@@ -13,7 +13,7 @@ function Header() {
     };
     let dropdownData = {label:"Language", list:["Spa","Hin","Fra","Jap"]};
     return <div class="mainHeader" >
-
+            <span className="heading-label">LangMap</span>
             <button className="btn btn-primary" type="button" style={{minWidth:'150px', marginBottom:"5px", marginRight:"5px"}} >
                 Search a word
             </button>
